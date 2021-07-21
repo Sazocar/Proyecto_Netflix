2b298ee104ecaf1e890ce904ed4f97a211940925
Simón A Azócar <saimn.azocar@gmail.com>
2021-07-21 19:06:18 -0400

    first commit

---
+++ Netflix/urls.py
@@ -0,0 +1,28 @@
+"""Netflix URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('suscripcion', views.suscripcion_1, name="suscripcion"),
+    path('suscripcion2', views.suscripcion_2, name="suscripcion2"),
+    path('select-genero', views.select_genero, name="select-genero"),
+    path('agregar-perfil', views.agregar_perfil, name="agregar-perfil"),
+    path('admin/',  admin.site.urls),
+]
---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+def suscripcion_1(request):
+    return render(request, "core/suscripcion_1.html")
+
+def suscripcion_2(request):
+    return render(request, "core/suscripcion_2.html")
+
+def select_genero(request):
+    return render(request, "core/select_genero.html")
+
+def agregar_perfil(request):
+    return render(request, "core/agregar_perfiles.html")
