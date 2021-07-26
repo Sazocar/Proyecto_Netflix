4d53ad12810edfc944a5db033182abe5fc74970f
Simón A Azócar <saimn.azocar@gmail.com>
2021-07-26 18:19:14 -0400

    agregada algunas validaciones

--- user/forms.py
+++ user/forms.py
@@ -10,6 +10,8 @@ class UserModelForm(forms.ModelForm):
         attrs={'placeholder': 'Escribe tu nombre'}), min_length=3, max_length=100)
     credit_card = forms.CharField(required=True, widget=forms.TextInput(
         attrs={'placeholder': 'Número de Tarjeta'}), min_length=13, max_length=13)
+    safe_code = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={'placeholder': 'Código de Seguridad'}), min_length=3, max_length=3)
 
 
     class Meta:
@@ -18,13 +20,42 @@ class UserModelForm(forms.ModelForm):
             'email',
             'password',
             'name',
-            'credit_card'
+            'credit_card',
+            'safe_code'
         ]
+
+    def clean_name(self):
+        data = self.cleaned_data.get('name')
+        if len(data) < 3:
+            raise forms.ValidationError("El nombre es muy corto, debe ser mayor a 3 caracteres")
+            return data
+        elif data.isalpha() is False:
+            raise forms.ValidationError(
+                "Nombre solo puede tener letras desde A-Z. No se aceptan numeros ni caracteres especiales")
+        return data
+
+    def clean_credit_card(self):
+        data = self.cleaned_data.get('credit_card')
+        if data.isalpha():
+            raise forms.ValidationError(
+                "Numero de tarjeta solo puede tener números enteros. No se aceptan letras ni caracteres especiales")
+        return data
+
+    def clean_safe_code(self):
+        data = self.cleaned_data.get('safe_code')
+        # data_as_int = int(data)
+        if data.isalpha():
+            raise forms.ValidationError(
+                "Código de seguridad solo puede tener números enteros. No se aceptan letras ni caracteres especiales")
+        return data
+        # elif (data_as_int < 0):
+        #     raise forms.ValidationError("Ingrese un codigo de seguridad positivo y entero de 3 digitos")
+        # return data
 
     # def clean_name(self):
     #     data = self.cleaned_data.get('name')
-    #     if len(data) < 5:
-    #         raise forms.ValidationError("El nombre es muy corto")
+    #     if data.isalpha() is False:
+    #         raise forms.ValidationError("Nombre solo puede tener letras desde A-Z. No se aceptan numeros ni caracteres especiales")
     #     return data
 
 
--- user/models.py
+++ user/models.py
@@ -5,11 +5,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(verbose_name="Email", max_length=254, null=False)
+    email = models.EmailField(
+        unique=True, verbose_name="Email", max_length=254, null=False)
     password = models.CharField(verbose_name="Contraseña", max_length=30)
     name = models.CharField(verbose_name="Nombre", max_length=50)
     credit_card = models.BigIntegerField(unique = True, verbose_name="Nro Tarjeta", null=False, 
                         validators=[MaxValueValidator(9999999999999), MinValueValidator(0)])
+    safe_code = models.IntegerField(verbose_name="Código de Seguridad", null=False, 
+                        validators=[MaxValueValidator(999), MinValueValidator(0)])
 
     class Meta:
         verbose_name = "usuario"
